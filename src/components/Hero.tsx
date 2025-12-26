d066d85c6278b7e3e9e413a5a6aed9601d4c393b
Kolade <olubanjok@gmail.com>
2025-12-26 19:02:19 +0000

    fix(hero): fix hats placement on mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -34,7 +34,7 @@ export default function Hero() {
           </Button>
         </div>
         <img
-          className=" absolute top-31 left-15 rotate-[-30deg] lg:top-16 lg:left-118 lg:block"
+          className=" absolute top-33 left-15 rotate-[-30deg] lg:top-16 lg:left-118 lg:block"
           src="/assets/images/bucket-hat2.png"
           alt="Bucket Hat"
           width={100}
@@ -42,7 +42,7 @@ export default function Hero() {
         />
 
         <img
-          className="absolute top-62 left-32 lg:top-60 lg:left-76 lg:block"
+          className="absolute top-69 left-32 lg:top-60 lg:left-76 lg:block"
           src="/assets/images/top-hat.png"
           alt="Top hat"
           width={100}
@@ -50,7 +50,7 @@ export default function Hero() {
         />
 
         <img
-          className=" absolute bottom-64 right-14 rotate-[-20deg] lg:bottom-55 lg:right-51 lg:block"
+          className=" absolute bottom-71 right-14 rotate-[-20deg] lg:bottom-55 lg:right-51 lg:block"
           src="/assets/images/snapback-hat2.png"
           alt="Snapback"
           width={100}
